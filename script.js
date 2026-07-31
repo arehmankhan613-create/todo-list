9c3f62e4cd6c16a5dbdc25f1ef5301dd506075dc
arehmankhan613-create <arehmankhan613@gmail.com>
2026-07-31 15:52:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask(){
+
+let task=document.getElementById("task").value;
+
+let li=document.createElement("li");
+
+li.innerText=task;
+
+document.getElementById("taskList").appendChild(li);
+
+document.getElementById("task").value="";
+
+}
